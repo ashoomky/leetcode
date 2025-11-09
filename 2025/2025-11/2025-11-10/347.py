a66a7e6b4344427547b1a130de68b0e111dab8ec
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-11-10 10:50:41 +1300

    top k frequent elements conquered

--- 2025/2025-11/2025-11-10/347.py
+++ 2025/2025-11/2025-11-10/347.py
@@ -5,4 +5,12 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        freq = {}
+        for n in nums:
+            if n in freq:
+                freq[n] += 1
+            else:
+                freq[n] = 1
+        sorted_freq = sorted(freq.items(), key=lambda x:x[1])
+        return [key[0] for key in sorted_freq[-k:]]
         
