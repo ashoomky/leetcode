58d7829ba04c61181cba53f3cd27000ab056ae73
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-11-14 15:34:19 +1300

    minstackkkk pretty fun heehee

---
+++ 2025/2025-11/2025-11-14/155.py
@@ -0,0 +1,56 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        elif val < self.min_stack[-1]:
+            self.min_stack.append(val)
+        else:
+            min_val = self.min_stack.pop()
+            self.min_stack.append(val)
+            self.min_stack.append(min_val)
+  
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        if self.min_stack[-1] == self.stack[-1]:
+            self.min_stack.pop()
+            self.stack.pop()
+        else:
+            min_val = self.min_stack.pop()
+            self.min_stack.pop()
+            self.min_stack.append(min_val)
+            self.stack.pop()
+ 
+
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min_stack[-1]
+        
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
