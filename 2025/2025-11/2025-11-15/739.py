640c51aadbf0e8c7e7e03721870cab06425b4208
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-11-15 15:54:49 +1300

    forgot to push daily temperatures code oops

---
+++ 2025/2025-11/2025-11-15/739.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        
+        stack = [] # store as pair: (temp, index)
+        result = [0] * len(temperatures)
+        for index, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                stack_temp, stack_index = stack.pop()
+                result[stack_index] = index - stack_index
+            stack.append((temp, index))
+        return result
