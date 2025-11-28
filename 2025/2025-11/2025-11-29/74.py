f63a85a7162ef27cfec2d2597d2dcd80270c72f7
ashoomky <ashlee@shum.co.nz>
2025-11-29 10:13:22 +1300

    searching in a 2d array using binary search

---
+++ 2025/2025-11/2025-11-29/74.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        no_rows, length_cols = len(matrix), len(matrix[0])
+        top, bottom = 0, no_rows - 1
+        while top <= bottom:
+            row = (top + bottom) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bottom = row - 1
+            else:
+                break
+        
+        if not (top <=bottom):
+            return False
+        row = (top + bottom) // 2
+        l, r = 0, length_cols - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
+
+
+        
