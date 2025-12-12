459242146aafb5f3ddfdb745ed11b7c77c2d15c9
ashoomky <ashlee@shum.co.nz>
2025-12-13 10:05:25 +1300

    valid bst

---
+++ 2025/2025-12/2025-12-13/98.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+
+        def dfs(node, left, right):
+            if not node:
+                return True
+            
+            if not (left < node.val < right):
+                return False
+            
+            return dfs(node.left, left, node.val) and dfs(node.right, node.val, right)
+
+        return dfs(root, float('-inf'), float('inf'))
+       
