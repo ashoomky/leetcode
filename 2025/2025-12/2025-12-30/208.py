dbca10df7e7049b3410884c1c6cd0d7e20caffd5
ashoomky <ashlee@shum.co.nz>
2025-12-30 18:53:59 +1300

    implement trie !!!

---
+++ 2025/2025-12/2025-12-30/208.py
@@ -0,0 +1,54 @@
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        current = self.root
+
+        for c in word:
+            if c not in current.children:
+                current.children[c] = TrieNode()
+            current = current.children[c]
+        current.end_of_word = True
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        current = self.root
+        for c in word:
+            if c not in current.children:
+                return False
+            current = current.children[c]
+        return current.end_of_word
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        current = self.root
+        for c in prefix:
+            if c not in current.children:
+                return False
+            current = current.children[c]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
