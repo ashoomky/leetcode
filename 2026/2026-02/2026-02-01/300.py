4363455fef459468ef718fa8c0883c3e18e8a6e2
ashoomky <ashlee@shum.co.nz>
2026-02-01 17:58:22 +1300

    length of longest increasing subsequence dp

---
+++ 2026/2026-02/2026-02-01/300.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        memo = [-1] * n
+
+        def dfs(i):
+            if memo[i] != -1:
+                return memo[i]
+
+            LIS = 1
+            for j in range(i + 1, n):
+                if nums[i] < nums[j]:
+                    LIS = max(LIS, 1 + dfs(j))
+
+            memo[i] = LIS
+            return LIS
+
+        return max(dfs(i) for i in range(n))
