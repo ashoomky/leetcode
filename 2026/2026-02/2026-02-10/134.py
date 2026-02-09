3aed0fc7e2d16f60811e6fdb126bc663b29b61e5
ashoomky <ashlee@shum.co.nz>
2026-02-10 10:38:49 +1300

    gas stationnnnn

---
+++ 2026/2026-02/2026-02-10/134.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        if sum(gas) < sum(cost):
+            return -1
+
+        total = 0 # total gas have currently
+        result = 0
+
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+            if total < 0:
+                total = 0
+                result = i + 1
+        return result
+            
+
+
+
+        
